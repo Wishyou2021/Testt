ca5ee8a7b71b2872b2f0fc241675f6197b7c9a14
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-12-24 16:22:38 +0800

    Update streamlit run app.py

--- streamlit run app.py
+++ streamlit run app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 # 标题&文字
-  st.title("Sentiment Analysis of Tweets about US Airlines")
-  st.markdown("This application is a Streamlit dashboard to analyze 
-               the sentiment of tweets")
+st.title("Sentiment Analysis of Tweets about US Airlines")
+st.markdown("This application is a Streamlit dashboard to analyze 
+             the sentiment of tweets")
 
